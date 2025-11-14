ecef69aafe2b18889eaf17da6ef1cdff7ad162ff
ErikDuthiersEY <Erik.Duthiers.Gutierrez@es.ey.com>
2025-11-14 15:28:19 +0100

    Fix environment configuration

--- src/core/pipeline.py
+++ src/core/pipeline.py
@@ -1,11 +1,13 @@
 from azure.ai.ml import Input, Output, command
 from azure.ai.ml.dsl import pipeline
 from pathlib import Path
+from azure.ai.ml.entities import Environment
 
-def build_eval_pipeline(env_path:str, datasets_path:str, config_path:str, compute:str):
+def build_eval_pipeline(env:Environment, datasets_path:str, config_path:str, compute:str):
+
     inference = command(
         code=str(Path(__file__).parents[1] / "components/inference"),
-        environment=env_path, 
+        environment=env, 
         command="python run_infer.py --datasets ${{inputs.datasets}} --output_dir ${{outputs.out_dir}} --config ${{inputs.config}}",
         inputs={"datasets": Input(type="uri_folder"), "config": Input(type="uri_file")},
         outputs={"out_dir": Output(type="uri_folder")},
@@ -13,16 +15,16 @@ def build_eval_pipeline(env_path:str, datasets_path:str, config_path:str, comput
 
     rule_based_metrics = command(
         code=str(Path(__file__).parents[1] / "components/rule_based_metrics"),
-        environment=env_path,
+        environment=env,
         command="python run_rule_based_metrics.py --raw_dir ${{inputs.raw_dir}} --out_dir ${{outputs.out_dir}} --config ${{inputs.config}}",
         inputs={"raw_dir": Input(type="uri_folder"), "config": Input(type="uri_file")},
         outputs={"out_dir": Output(type="uri_folder")},
     )
 
     aggregate = command(
-        code=str(Path(__file__).parents[1] / "components/aggregate"),
-        environment=env_path,
-        command="python run_aggregate.py --scores_dir ${{inputs.scores}} --out_dir ${{outputs.out_dir}}",
+        code=str(Path(__file__).parents[1] / "components/aggregation"),
+        environment=env,
+        command="python run_aggregate.py --scores_dir ${{inputs.scores_dir}} --out_dir ${{outputs.out_dir}}",
         inputs={"scores_dir": Input(type="uri_folder")},
         outputs={"out_dir": Output(type="uri_folder")},
     )
@@ -31,7 +33,7 @@ def build_eval_pipeline(env_path:str, datasets_path:str, config_path:str, comput
     def _pipeline(datasets_folder: Input, config_file: Input) -> None:
         infer_job = inference(datasets=datasets_folder, config=config_file)
         rule_based_metrics_job = rule_based_metrics(raw_dir=infer_job.outputs.out_dir, config=config_file)
-        agg_job = aggregate(scores=rule_based_metrics_job[0].outputs.out_dir)
+        agg_job = aggregate(scores_dir=rule_based_metrics_job.outputs.out_dir)
         return {"summary": agg_job.outputs.out_dir}
 
     pl = _pipeline(
