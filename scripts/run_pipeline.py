07582a28469d170a3be953da159b9722fe950b8e
ErikDuthiersEY <Erik.Duthiers.Gutierrez@es.ey.com>
2025-11-14 14:20:13 +0100

    Fix config paths in pipeline definition

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -10,9 +10,9 @@ WS  = os.environ["AZ_ML_WORKSPACE"]
 ml = MLClient(DefaultAzureCredential(), SUB, RG, WS)
 
 env_path     = "configs/env/conda.yaml"
-datasets_dir = "datasets"
+datasets_dir = "azureml://subscriptions/3bcd6b3d-11a3-4a3e-9e9e-7beb46101ac4/resourcegroups/ws-multi-studio/workspaces/multistudioaml/datastores/workspaceblobstore/paths/LocalUpload/64c8c30cac2dd93c303d3bee5abdfa74d3e1cbf38e6e005a9dbf9c2e6371590c/"
 config_path  = "configs/config.yaml"
-compute      = "" #include compute name
+compute      = "metaverse-compute" 
 
 run = build_eval_pipeline(env_path, datasets_dir, config_path, compute)
 run.display_name = yaml.safe_load(open(config_path))["run_name"]
--- src/core/pipeline.py
+++ src/core/pipeline.py
@@ -6,7 +6,7 @@ def build_eval_pipeline(env_path:str, datasets_path:str, config_path:str, comput
     inference = command(
         code=str(Path(__file__).parents[1] / "components/inference"),
         environment=env_path, 
-        command="python infer.py --datasets ${{inputs.datasets}} --output_dir ${{outputs.out_dir}} --config ${{inputs.config}}",
+        command="python run_infer.py --datasets ${{inputs.datasets}} --output_dir ${{outputs.out_dir}} --config ${{inputs.config}}",
         inputs={"datasets": Input(type="uri_folder"), "config": Input(type="uri_file")},
         outputs={"out_dir": Output(type="uri_folder")},
     )
@@ -14,23 +14,23 @@ def build_eval_pipeline(env_path:str, datasets_path:str, config_path:str, comput
     rule_based_metrics = command(
         code=str(Path(__file__).parents[1] / "components/rule_based_metrics"),
         environment=env_path,
-        command="python metrics.py --raw_dir ${{inputs.raw_dir}} --out_dir ${{outputs.out_dir}}",
-        inputs={"raw_dir": Input(type="uri_folder")},
+        command="python run_rule_based_metrics.py --raw_dir ${{inputs.raw_dir}} --out_dir ${{outputs.out_dir}} --config ${{inputs.config}}",
+        inputs={"raw_dir": Input(type="uri_folder"), "config": Input(type="uri_file")},
         outputs={"out_dir": Output(type="uri_folder")},
     )
 
     aggregate = command(
         code=str(Path(__file__).parents[1] / "components/aggregate"),
         environment=env_path,
-        command="python aggregate.py --scores ${{inputs.scores}} --out_dir ${{outputs.out_dir}}",
-        inputs={"scores": Input(type="uri_folder")},
+        command="python run_aggregate.py --scores_dir ${{inputs.scores}} --out_dir ${{outputs.out_dir}}",
+        inputs={"scores_dir": Input(type="uri_folder")},
         outputs={"out_dir": Output(type="uri_folder")},
     )
 
     @pipeline() 
     def _pipeline(datasets_folder: Input, config_file: Input) -> None:
         infer_job = inference(datasets=datasets_folder, config=config_file)
-        rule_based_metrics_job = rule_based_metrics(raw_dir=infer_job.outputs.out_dir),
+        rule_based_metrics_job = rule_based_metrics(raw_dir=infer_job.outputs.out_dir, config=config_file)
         agg_job = aggregate(scores=rule_based_metrics_job[0].outputs.out_dir)
         return {"summary": agg_job.outputs.out_dir}
 
